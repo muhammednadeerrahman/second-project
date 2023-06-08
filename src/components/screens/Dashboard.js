777d392d1747b09d4056fe4795ed396cb9996ef6
muhammednadeerrahman <mohdnadeerrahman@gmail.com>
2023-06-08 13:09:58 +0530

    dashboard left top section structuring finished

--- src/components/screens/Dashboard.js
+++ src/components/screens/Dashboard.js
@@ -11,41 +11,66 @@ export default function Dashboard() {
                 <DashboardLeft>
                     <LeftTop>
                         <TopThreeContainer>
-                        <TopInnerTop>
-                            <TopCount>852,00</TopCount>
-                            <TopImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></TopImage>
-                            <Percentage>-10%</Percentage>
-                        </TopInnerTop>
-                        <TopInnerBottom>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
+                            <TopInnerTop>
+                                <TopCount>852,00</TopCount>
+                                <UpImageContainer>
+                                    <SpanContainer>
+                                    <TopImage src = {require("../assets/greenup.svg").default} alt = "image"></TopImage>
+                                    </SpanContainer>
+    
+                                    <Percentage>-10%</Percentage>
+                                </UpImageContainer>
+                            
+                            </TopInnerTop>
+                            <TopInnerBottom>
+                                <SymbolSpan>
+                                    <BottomImage src = {require("../assets/diamond.svg").default} alt = "image"/>
+                                </SymbolSpan>                            
+                                <GraphImage src = {require("../assets/graph.svg").default} alt = "image"/>
 
-                        </TopInnerBottom>
+                            </TopInnerBottom>
                         </TopThreeContainer>
                         <TopThreeContainer>
-                        <TopInnerTop>
-                            <TopCount>852,00</TopCount>
-                            <TopImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></TopImage>
-                            <Percentage>-10%</Percentage>
-                        </TopInnerTop>
-                        <TopInnerBottom>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
+                            <TopInnerTop>
+                                <TopCount>852,00</TopCount>
+                                <UpImageContainer>
+                                    <SpanContainerRed>
+                                    <TopImage src = {require("../assets/reddown.svg").default} alt = "image"></TopImage>
+                                    </SpanContainerRed>
+    
+                                    <PercentageRed>-10%</PercentageRed>
+                                </UpImageContainer>
+                            
+                            </TopInnerTop>
+                            <TopInnerBottom>
+                                <SymbolSpan>
+                                    <BottomImage src = {require("../assets/coinwater.svg").default} alt = "image"/>
+                                </SymbolSpan>                            
+                                <GraphImage src = {require("../assets/graph.svg").default} alt = "image"/>
 
-                        </TopInnerBottom>
+                            </TopInnerBottom>
                         </TopThreeContainer>
                         <TopThreeContainer>
-                        <TopInnerTop>
-                            <TopCount>852,00</TopCount>
-                            <TopImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></TopImage>
-                            <Percentage>-10%</Percentage>
-                        </TopInnerTop>
-                        <TopInnerBottom>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
-                            <BottomImage src = {require("../assets/Property-1=down.svg").default} alt = "image"></BottomImage>
+                            <TopInnerTop>
+                                <TopCount>852,00</TopCount>
+                                <UpImageContainer>
+                                    <SpanContainer>
+                                    <TopImage src = {require("../assets/greenup.svg").default} alt = "image"></TopImage>
+                                    </SpanContainer>
+    
+                                    <Percentage>-10%</Percentage>
+                                </UpImageContainer>
+                            
+                            </TopInnerTop>
+                            <TopInnerBottom>
+                                <SymbolSpan>
+                                    <BottomImage src = {require("../assets/bitcoin.svg").default} alt = "image"/>
+                                </SymbolSpan>                            
+                                <GraphImage src = {require("../assets/graph.svg").default} alt = "image"/>
 
-                        </TopInnerBottom>
+                            </TopInnerBottom>
                         </TopThreeContainer>
+                 
                         
                     </LeftTop>
                     <DashboardMiddleSection>
@@ -103,7 +128,7 @@ export default function Dashboard() {
                             </BitCoinContainer>
                             <BitCoinContainer>
                                 <BitCoinLeftContainer>
-                                    <BitcoinImage src = {require("../assets/coinwaterbitcoin.svg").default} alt = "BitcoinImage" />
+                                    <BitcoinImage src = {require("../assets/coinwater.svg").default} alt = "BitcoinImage" />
                                     <BitCoinTitleContainer>
                                         <NameCoin>Bitcoin</NameCoin>
                                         <NameCoinType>BTC</NameCoinType>
@@ -132,7 +157,7 @@ export default function Dashboard() {
                     <RightBottomSection>
                         <RightBottomInsideTop>
                             <LeftExchangeTitle>Exchange</LeftExchangeTitle>
-                            <RightSyncImage src = {require("../assets/.svg").default} alt = "syncImage" />
+                            <RightSyncImage src = {require("../assets/sync.svg").default} alt = "syncImage" />
                         </RightBottomInsideTop>
                         <RightBottomInsideMiddle>
                             
@@ -141,28 +166,29 @@ export default function Dashboard() {
                                 <SaleAmount>$678</SaleAmount>
 			                    <SaleAmountRightContaner>
                                     <Currency>USD</Currency>
-				                    <CurrencyDropdown src = {require("../assets/.svg").default} alt = "dropdownImage" />
+				                    <CurrencyDropdown></CurrencyDropdown>
 
                                 </SaleAmountRightContaner>
                             </SaleContainer>
-                            <BuyName>Buy</BuyName>
-		                    <BuyContainer>
-                                <BuyAmount>$678</BuyAmount>
-			                    <BuyAmountRightContaner>
-                                    <Currency>USD</Currency>
-				                    <CurrencyDropdown src = {require("../assets/.svg").default} alt = "dropdownImage" />
+                            <SaleName>Sale</SaleName>
+		                    <SaleContainer>
+                                <SaleAmount>.990</SaleAmount>
+			                    <SaleAmountRightContaner>
+                                    <Currency>ETH</Currency>
+				                    <CurrencyDropdown></CurrencyDropdown>
 
-                                </BuyAmountRightContaner>
-                            </BuyContainer>
+                                </SaleAmountRightContaner>
+                            </SaleContainer>
 
                         </RightBottomInsideMiddle>
 
                     </RightBottomSection>
+                    <RightBottomSubmitButton>Exchange Now</RightBottomSubmitButton>
 
 
  
 
-	<RightBottomSubmitButton>
+	
 
 
                 </DashboardRight>
@@ -176,26 +202,102 @@ const SectionDashboard = styled.div`
 background-color:  #111219;
 `
 const SectionWrapper = styled.div`
+display: flex;
+justify-content: space-between;
 max-width :1200px;
 width: 90%;
 margin: 0 auto;
 `
-const DashboardLeft = styled.div``
+const DashboardLeft = styled.div`
+width:70%;
+`
 const LeftTop = styled.div`
 display: flex;
 justify-content: space-between;
 `
 const TopThreeContainer = styled.div`
-width: 30%;
+border-radius: 8px;
+padding: 20px;
+width: 25%;
 background-color: #171B2C ;
+margin: 60px 0 50px;
 `
-const TopInnerTop = styled.div``
-const TopCount = styled.h4``
-const TopImage = styled.img``
-const TopInnerBottom = styled.div``
-const BottomImage = styled.img``
-const DashboardRight = styled.div``
-const Percentage = styled.span``
+const TopInnerTop = styled.div`
+display: flex;
+justify-content: space-between;
+`
+const TopCount = styled.h4`
+color: #fff;
+`
+const UpImageContainer = styled.div`
+display: flex;
+`
+const SpanContainer = styled.span`
+background-color: rgba(24,44,50,255);
+border-radius: 50%;
+width: 25px;
+height: 25px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-right: 5px;
+`
+const SpanContainerRed = styled.span`
+background-color: #2d1b31;
+border-radius: 50%;
+width: 25px;
+height: 25px;
+display: flex;
+justify-content: center;
+align-items: center;
+margin-right: 5px;
+`
+const TopImage = styled.img`
+width: 15px;
+height: 15px;
+display: block;
+
+`
+const TopInnerBottom = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+margin-top: 20px;
+`
+const SymbolSpan = styled.span`
+border-radius: 50%;
+background-color: #111219;
+width: 30px;
+height: 30px;
+display: flex;
+justify-content: center;
+align-items: center;
+
+`
+const BottomImage = styled.img`
+width: 20px;
+height: 20px;
+display: block;
+`
+const GraphImage = styled.img`
+width: 40px;
+height: 40px;
+display: block;
+`
+const DashboardRight = styled.div`
+width:35%;
+
+`
+const Percentage = styled.span`
+color: #07af5a;
+display: block;
+`
+const PercentageRed = styled.span`
+color: #07af5a;
+display: block;
+color: #983752;
+`
+
 const DashboardMiddleSection = styled.div`
 width:100%;
 height: 300px;
@@ -210,6 +312,51 @@ width:40%;
 height: 100px;
 background:#171B2C;
 `
+const RightTopContainer = styled.div`
+background-color: #171B2C ;
+`
+const TopInsideTop = styled.div``
+const InsideTopLeft = styled.div``
+const Amount = styled.h4``
+const InsideTopRight = styled.div``
+const TopInsideImage = styled.img``
+const RightTopMiddle = styled.div``
+const MiddleTopInside = styled.div``
+const TotalAmount = styled.h3``
+const MiddleBottomInside = styled.div``
+const DepositContainer = styled.div``
+const DepositImage = styled.img``
+const Deposit = styled.h4``
+const WithdrawelContainer = styled.div``
+const WithdrawImage = styled.img``
+const Withdraw = styled.h4``
+const RightTopBottom = styled.div``
+const TransactionTitle = styled.h5``
+const BitCoinContainer = styled.div``
+const BitCoinLeftContainer = styled.div``
+const BitcoinImage = styled.img``
+const BitCoinTitleContainer = styled.div``
+const NameCoin = styled.h4``
+const NameCoinType = styled.h4``
+const BitCoinRightContainer = styled.div``
+const BitcoinAmount = styled.h4``
+const BitcoinTime = styled.h4``
+const RightBottomSection = styled.div`
+background-color: #171B2C ;
+`
+const RightBottomInsideTop = styled.div``
+const LeftExchangeTitle = styled.h3``
+const RightSyncImage = styled.img``
+const RightBottomInsideMiddle = styled.div``
+const SaleName = styled.h3``
+const SaleContainer = styled.div``
+const SaleAmount = styled.h4``
+const SaleAmountRightContaner = styled.div``
+const Currency = styled.h4``
+const CurrencyDropdown = styled.select``
+const RightBottomSubmitButton = styled.button``
+
+
 
 
 
